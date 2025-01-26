6d4d8acd16254f338e2057059d303cdff2f5e7dc
dozyio <github@dozy.io>
2025-01-26 02:53:56 +0000

    fmt contract

--- contracts/Minimal.sol
+++ contracts/Minimal.sol
@@ -2,6 +2,6 @@
 pragma solidity ^0.8.19;
 
 contract Minimal {
-  // This is enough for the contract to accept ETH
-  receive() external payable {}
+    // This is enough for the contract to accept ETH
+    receive() external payable {}
 }
